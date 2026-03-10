8eddffc2cab32cb16812a496a9093b52948c30d7
Ishan Rai <ishan.rai1907@gmail.com>
2026-03-10 13:59:13 +0530

    added central linker and SCD cluster in validate_domains.py

--- src/utils/validate_domains.py
+++ src/utils/validate_domains.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48d66353f2d95f3b5accc4081ec03e4dad30e09e3e826edee3ebde51a274d690
-size 3363
+oid sha256:dfa510239ac64fa5654b906ded89e15ddfa0ad492c8ceea46079739669d59267
+size 4092
